0d2a203e2272bd01d0270d43db8eaea00dd6f1db
ninvit <ninvit@gmail.com>
2020-12-23 17:15:10 -0300

    Testing

---
+++ src/test/java/net/intelie/challenges/EventInsertTest.java
@@ -0,0 +1,19 @@
+package net.intelie.challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDateTime;
+
+public class EventInsertTest {
+    @Test
+    public void thisIsAWarning() throws Exception {
+        Event event = new Event("some_type", LocalDateTime.now());
+        LocalDateTime testDate = event.getTimestamp();
+
+        System.out.println("Testing auto created Date " + testDate);
+        assertEquals(testDate, event.getTimestamp());
+        assertEquals("some_type", event.getType());
+    }
+}
