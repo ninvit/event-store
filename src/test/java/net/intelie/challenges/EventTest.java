3885c39edb6e54af1f91449e35bc90432fd480ed
ninvit <ninvit@gmail.com>
2020-12-24 14:36:54 -0300

    testing improvements

--- src/test/java/net/intelie/challenges/EventTest.java
+++ src/test/java/net/intelie/challenges/EventTest.java
@@ -2,31 +2,54 @@ package net.intelie.challenges;
 
 import org.junit.Test;
 
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class EventTest {
+
+    EventStore eventStore = new EventStoreImpl();
+
+    LocalDateTime testDateTime = LocalDateTime.now();
+
     @Test
     public void insertEvent() throws Exception {
-
-        EventStore eventStore = new EventStoreImpl();
-
-        LocalDateTime testDateTime = LocalDateTime.now();
 
         Event event = new Event();
         event.setType("TestType");
         event.setTimestamp(testDateTime);
 
-        eventStore.insert(event);
-
         Event event2 = new Event();
         event2.setType("TestType2");
         event2.setTimestamp(testDateTime);
 
-        eventStore.insert(event2);
+        try {
+            System.out.println("Testing event insertion: " + event.getType());
+            eventStore.insert(event);
+            
+            System.out.println("Testing event insertion: " + event2.getType());
+            eventStore.insert(event2);
 
-        System.out.println(eventStore.getEvents().get(0).getType());
-        
-        System.out.println(eventStore.getEvents().get(1).getType());
+            System.out.println(eventStore.getEvents().size() + " events where inserted");
+
+        } catch (Exception e) {
+            System.out.println("error " + e.getMessage());
+        }
+
+        assertThat(eventStore.getEvents()).isNotNull();
+        assertThat(eventStore.getEvents().get(0)).isNotNull();
+        assertThat(eventStore.getEvents().get(0).getType()).isEqualTo(event.getType());
+
+        assertThat(eventStore.getEvents().get(1)).isNotNull();
+        assertThat(eventStore.getEvents().get(1).getType()).isEqualTo(event2.getType());
+    }
+
+    @Test
+    public void listEvents() throws Exception {
+
+        System.out.println("List testing");
 
     }
 }
